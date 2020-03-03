53fc5123f8cdfd510a08af8aed08874ade4bf626
Edward Napierala <en322@cl.cam.ac.uk>
2020-03-03 18:37:03 +0000

    Fix build somewhat.

--- tcg/tcg-op.c
+++ tcg/tcg-op.c
@@ -3212,7 +3212,7 @@ void tcg_gen_atomic_cmpxchg_i32_with_checked_addr(
             tcg_temp_free_i32(oi);
         }
 #else
-        gen(retv, cpu_env, addr, cmpv, newv);
+        gen(retv, cpu_env, checked_addr, cmpv, newv);
 #endif
 
         if (memop & MO_SIGN) {
@@ -3342,7 +3342,7 @@ static void do_atomic_op_i32(TCGv_i32 ret, TCGv_cap_checked_ptr checked_addr,
         tcg_temp_free_i32(oi);
     }
 #else
-    gen(ret, cpu_env, addr, val);
+    gen(ret, cpu_env, checked_addr, val);
 #endif
 #if defined(TARGET_MIPS) || defined(TARGET_RISCV)
     TCGv_i32 op = tcg_const_i32(memop);
