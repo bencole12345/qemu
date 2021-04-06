5f3a916e25e2ff62ff5055ccf59d36648010e6d1
Ben Cole <bencole12345@gmail.com>
2021-04-06 08:37:05 +0100

    Implement CSetStackFrameSizeImm instruction + bug fixes

--- target/cheri-common/cheri-compressed-cap/cheri_compressed_cap_common.h
+++ target/cheri-common/cheri-compressed-cap/cheri_compressed_cap_common.h
@@ -341,6 +341,7 @@ static inline void _cc_N(decompress_raw)(_cc_addr_t pesbt, _cc_addr_t cursor, bo
         _cc_debug_assert(cdp->_cr_top <= _CC_N(MAX_TOP));
         _cc_debug_assert(cdp->cr_base <= cdp->_cr_top);
         _cc_debug_assert(cdp->cr_reserved == 0);
+        _cc_debug_assert(cdp->cr_stack_frame_size <= 7);
     }
 }
 
@@ -679,11 +680,14 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
 
     // TODO: find a faster way to compute top and bot:
     const _cc_addr_t pesbt = _CC_ENCODE_FIELD(0, UPERMS) | _CC_ENCODE_FIELD(0, HWPERMS) |
-                             _CC_ENCODE_FIELD(_CC_N(OTYPE_UNSEALED), OTYPE) | _CC_ENCODE_FIELD(new_ebt, EBT);
+                             _CC_ENCODE_FIELD(_CC_N(OTYPE_UNSEALED), OTYPE) |
+                             _CC_ENCODE_FIELD(cap->cr_stack_frame_size, STACK_FRAME_SIZE) |
+                             _CC_ENCODE_FIELD(new_ebt, EBT);
     _cc_cap_t new_cap;
     _cc_N(decompress_raw)(pesbt, cursor, cap->cr_tag, &new_cap);
     _cc_addr_t new_base = new_cap.cr_base;
     _cc_length_t new_top = new_cap._cr_top;
+    uint8_t new_stack_frame_size = new_cap.cr_stack_frame_size;
     if (exact) {
         _cc_debug_assert(new_base == req_base && "Should be exact");
         _cc_debug_assert(new_top == req_top && "Should be exact");
@@ -698,6 +702,7 @@ static inline bool _cc_N(setbounds_impl)(_cc_cap_t* cap, _cc_addr_t req_base, _c
     cap->_cr_cursor = cursor; // ensure that the address is correct
     cap->_cr_top = new_top;
     cap->cr_ebt = new_ebt;
+    cap->cr_stack_frame_size = new_stack_frame_size;
     // TODO: update pesbt?
     //  let newCap = {cap with address=base, E=to_bits(6, if incE then e + 1 else e), B=Bbits, T=Tbits, internal_e=ie};
     //  (exact, newCap)
